c18b8adf89b2baf12b45ee863f9fd2268d1617ce
user <wkdrngla789@naver.com>
2022-09-19 23:57:54 +0900

    0919

--- practice/src/com/lev2/No2525.java
+++ practice/src/com/lev2/No2525.java
@@ -1,5 +1,19 @@
 package com.lev2;
 
-public class No2525 {
+import java.util.Scanner;
 
+public class No2525 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int ab = 60*a+b;
+		ab += c;
+		
+		int h = (ab/60)%24;
+		int m = ab%60;
+		System.out.println(h+" "+m);
+	}
 }
