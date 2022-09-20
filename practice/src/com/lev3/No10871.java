1e271b2cc2ff66fcc8051f34747eb3a85c41176b
user <wkdrngla789@naver.com>
2022-09-20 23:54:41 +0900

    0920

---
+++ practice/src/com/lev3/No10871.java
@@ -0,0 +1,9 @@
+package com.lev3;
+
+
+public class No10871 {
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ practice/src/com/lev3/No11022.java
@@ -0,0 +1,18 @@
+package com.lev3;
+
+import java.util.Scanner;
+
+public class No11022 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int a;
+		int b;
+		
+		for(int i=1; i<=t; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+		}
+	}
+}
